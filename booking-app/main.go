ac2fed1293d21efa93429247009a5e7da377ec60
mark0v <amark0vv@gmail.com>
2023-11-28 18:33:45 +0200

    add welcome logic

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,1 +1,40 @@
-Print("Hello World")
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Wolcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var bookings [50]string
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets = 2
+
+	fmt.Println("Enter your Name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your Last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaning for %v\n", remainingTickets, conferenceName)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mark0v/booking-app
+
+go 1.21.3
